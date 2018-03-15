f5ac7c190e06ae1eee4844814a8fcad973ad245a
=Julian Schäfer <jschaefer97@gmx.de>
2018-03-15 21:23:41 +0100

    Fixed not working test

--- src/test/groovy/com/gildedrose/GildedRoseSpec.groovy
+++ src/test/groovy/com/gildedrose/GildedRoseSpec.groovy
@@ -10,16 +10,25 @@ class GildedRoseSpec extends Specification {
     def "should update quality correctly"() {
 
         given: "some items"
-        Item[] items = [new Item("foo", 0, 0)];
+        Item[] items = [new Item("+5 Dexterity Vest", 10, 20), //
+                        new Item("Aged Brie", 2, 0)] //]
 
         and: "the application with these items"
-        GildedRose app = new GildedRose(items);
+        GildedRose app = new GildedRose(items)
 
         when: "updating quality"
-        app.updateQuality();
+        app.updateQuality()
 
-        then: "the quality is correct"
-        app.items[0].name == "fixme"
+        then: "The list is as expected"
+        app.items[0].name == "+5 Dexterity Vest"
+        app.items[1].name == "Aged Brie"
+
+        and: "The items properties are as expected"
+        app.items[0].quality == 19
+        app.items[0].sellIn == 9
+        app.items[1].quality == 1
+        app.items[1].sellIn == 1
+
     }
 
 }
